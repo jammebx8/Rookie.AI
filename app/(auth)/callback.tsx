b91f86d1eea964282a388435dbbe3caf9cba2468
nkbta <hgggffdfaaa@gmail.com>
2025-11-30 23:30:57 +0530

    last fucking time

--- app/(auth)/callback.tsx
+++ app/(auth)/callback.tsx
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from 'expo-router';
 import { useEffect } from 'react';
 import { ActivityIndicator, Text, View } from 'react-native';
-import { supabase } from '../src/utils/supabase';
+import { supabase } from '../../src/utils/supabase';
 import React from 'react';
 
 export default function AuthCallback() {
